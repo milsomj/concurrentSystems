73ebef22ba14031545725cb148f4a4f4ebc01dfc
Eoghan Quinn <quinne16@lg12l20.scss.tcd.ie>
2017-03-15 22:09:29 +0000

    brute force approach through 1,3,5 & 7; theres a small error within acceptable range

--- conv-harness.c
+++ conv-harness.c
@@ -121,39 +121,39 @@ float **** copy_4d_matrix(float **** source_matrix, int dim0,
 /* create a matrix and fill it with random numbers */
 float **** gen_random_4d_matrix(int dim0, int dim1, int dim2, int dim3)
 {
-float **** result;
-int i, j, k, l;
-struct timeval seedtime;
-  int seed;
+  float **** result;
+  int i, j, k, l;
+  struct timeval seedtime;
+    int seed;
 
-  result = new_empty_4d_matrix(dim0, dim1, dim2, dim3);
+    result = new_empty_4d_matrix(dim0, dim1, dim2, dim3);
 
-  /* use the microsecond part of the current time as a pseudorandom seed */
-  gettimeofday(&seedtime, NULL);
-  seed = seedtime.tv_usec;
-  srandom(seed);
+    /* use the microsecond part of the current time as a pseudorandom seed */
+    gettimeofday(&seedtime, NULL);
+    seed = seedtime.tv_usec;
+    srandom(seed);
 
-  /* fill the matrix with random numbers */
-  const int range = 1 << 12; // 2^12
-  const int bias = 1 << 16; // 2^16
-  float offset = 0.0;
-  for ( i = 0; i < dim0; i++ ) {
-    for ( j = 0; j < dim1; j++ ) {
-      for ( k = 0; k < dim2; k++ ) {
-        for ( l = 0; l < dim3; l++ ) {
-          // generate uniform random integer with mean of zero
-          long long rand = random();
-          // now cut down the range and bias the mean to reduce
-          // the likelihood of large floating point round-off errors
-          int reduced_range = (rand % range);
-          float num = (((float) reduced_range) / ((float) bias))+offset;
-          result[i][j][k][l] = num;
+    /* fill the matrix with random numbers */
+    const int range = 1 << 12; // 2^12
+    const int bias = 1 << 16; // 2^16
+    float offset = 0.0;
+    for ( i = 0; i < dim0; i++ ) {
+      for ( j = 0; j < dim1; j++ ) {
+        for ( k = 0; k < dim2; k++ ) {
+          for ( l = 0; l < dim3; l++ ) {
+            // generate uniform random integer with mean of zero
+            long long rand = random();
+            // now cut down the range and bias the mean to reduce
+            // the likelihood of large floating point round-off errors
+            int reduced_range = (rand % range);
+            float num = (((float) reduced_range) / ((float) bias))+offset;
+            result[i][j][k][l] = num;
+          }
         }
       }
     }
-  }
 
-  return result;
+    return result;
 }
 
 /* create a matrix and fill it with random numbers */
@@ -228,10 +228,34 @@ void team_conv(float *** image, float **** kernels, float *** output,
                int width, int height, int nchannels, int nkernels,
                int kernel_order)
 {
-  // this call here is just dummy code
-  // insert your own code instead
-  multichannel_conv(image, kernels, output, width,
-                    height, nchannels, nkernels, kernel_order);
+  int h, w, x, y, c, m;
+
+  for ( m = 0; m < nkernels; m++ ) {
+    for ( w = 0; w < width; w++ ) {
+      for ( h = 0; h < height; h++ ) {
+        double sum = 0.0;
+        for ( c = 0; c < nchannels; c++ ) {
+            if(kernel_order==1){
+              sum += image[w][h][c] * kernels[m][c][0][0];
+            }
+            else{
+              if(kernel_order==3){
+                sum += (image[w][h][c] * kernels[m][c][0][0]) + (image[w][h+1][c] * kernels[m][c][0][1]) + (image[w][h+2][c] * kernels[m][c][0][2]) + (image[w+1][h][c] * kernels[m][c][1][0]) + (image[w+1][h+1][c] * kernels[m][c][1][1]) + (image[w+1][h+2][c] * kernels[m][c][1][2]) + (image[w+2][h][c] * kernels[m][c][2][0]) + (image[w+2][h+1][c] * kernels[m][c][2][1]) + (image[w+2][h+2][c] * kernels[m][c][2][2]);
+              }
+              else {
+                if(kernel_order==5){
+                  sum += (image[w+0][h+0][c] * kernels[m][c][0][0]) + (image[w+0][h+1][c] * kernels[m][c][0][1]) + (image[w+0][h+2][c] * kernels[m][c][0][2]) + (image[w+0][h+3][c] * kernels[m][c][0][3]) + (image[w+0][h+4][c] * kernels[m][c][0][4]) + (image[w+1][h+0][c] * kernels[m][c][1][0]) + (image[w+1][h+1][c] * kernels[m][c][1][1]) + (image[w+1][h+2][c] * kernels[m][c][1][2]) + (image[w+1][h+3][c] * kernels[m][c][1][3]) + (image[w+1][h+4][c] * kernels[m][c][1][4]) + (image[w+2][h+0][c] * kernels[m][c][2][0]) + (image[w+2][h+1][c] * kernels[m][c][2][1]) + (image[w+2][h+2][c] * kernels[m][c][2][2]) + (image[w+2][h+3][c] * kernels[m][c][2][3]) + (image[w+2][h+4][c] * kernels[m][c][2][4]) + (image[w+3][h+0][c] * kernels[m][c][3][0]) + (image[w+3][h+1][c] * kernels[m][c][3][1]) + (image[w+3][h+2][c] * kernels[m][c][3][2]) + (image[w+3][h+3][c] * kernels[m][c][3][3]) + (image[w+3][h+4][c] * kernels[m][c][3][4]) + (image[w+4][h+0][c] * kernels[m][c][4][0]) + (image[w+4][h+1][c] * kernels[m][c][4][1]) + (image[w+4][h+2][c] * kernels[m][c][4][2]) + (image[w+4][h+3][c] * kernels[m][c][4][3]) + (image[w+4][h+4][c] * kernels[m][c][4][4]);
+                }
+                else{
+                  sum += (image[w+0][h+0][c] * kernels[m][c][0][0]) + (image[w+0][h+1][c] * kernels[m][c][0][1]) + (image[w+0][h+2][c] * kernels[m][c][0][2]) + (image[w+0][h+3][c] * kernels[m][c][0][3]) + (image[w+0][h+4][c] * kernels[m][c][0][4]) + (image[w+0][h+5][c] * kernels[m][c][0][5]) + (image[w+0][h+6][c] * kernels[m][c][0][6]) + (image[w+1][h+0][c] * kernels[m][c][1][0]) + (image[w+1][h+1][c] * kernels[m][c][1][1]) + (image[w+1][h+2][c] * kernels[m][c][1][2]) + (image[w+1][h+3][c] * kernels[m][c][1][3]) + (image[w+1][h+4][c] * kernels[m][c][1][4]) + (image[w+1][h+5][c] * kernels[m][c][1][5]) + (image[w+1][h+6][c] * kernels[m][c][1][6]) + (image[w+2][h+0][c] * kernels[m][c][2][0]) + (image[w+2][h+1][c] * kernels[m][c][2][1]) + (image[w+2][h+2][c] * kernels[m][c][2][2]) + (image[w+2][h+3][c] * kernels[m][c][2][3]) + (image[w+2][h+4][c] * kernels[m][c][2][4]) + (image[w+2][h+5][c] * kernels[m][c][2][5]) + (image[w+2][h+6][c] * kernels[m][c][2][6]) + (image[w+3][h+0][c] * kernels[m][c][3][0]) + (image[w+3][h+1][c] * kernels[m][c][3][1]) + (image[w+3][h+2][c] * kernels[m][c][3][2]) + (image[w+3][h+3][c] * kernels[m][c][3][3]) + (image[w+3][h+4][c] * kernels[m][c][3][4]) + (image[w+3][h+5][c] * kernels[m][c][3][5]) + (image[w+3][h+6][c] * kernels[m][c][3][6]) + (image[w+4][h+0][c] * kernels[m][c][4][0]) + (image[w+4][h+1][c] * kernels[m][c][4][1]) + (image[w+4][h+2][c] * kernels[m][c][4][2]) + (image[w+4][h+3][c] * kernels[m][c][4][3]) + (image[w+4][h+4][c] * kernels[m][c][4][4]) + (image[w+4][h+5][c] * kernels[m][c][4][5]) + (image[w+4][h+6][c] * kernels[m][c][4][6]) + (image[w+5][h+0][c] * kernels[m][c][5][0]) + (image[w+5][h+1][c] * kernels[m][c][5][1]) + (image[w+5][h+2][c] * kernels[m][c][5][2]) + (image[w+5][h+3][c] * kernels[m][c][5][3]) + (image[w+5][h+4][c] * kernels[m][c][5][4]) + (image[w+5][h+5][c] * kernels[m][c][5][5]) + (image[w+5][h+6][c] * kernels[m][c][5][6]) + (image[w+6][h+0][c] * kernels[m][c][6][0]) + (image[w+6][h+1][c] * kernels[m][c][6][1]) + (image[w+6][h+2][c] * kernels[m][c][6][2]) + (image[w+6][h+3][c] * kernels[m][c][6][3]) + (image[w+6][h+4][c] * kernels[m][c][6][4]) + (image[w+6][h+5][c] * kernels[m][c][6][5]) + (image[w+6][h+6][c] * kernels[m][c][6][6]);
+                }
+              }
+            }
+          output[m][w][h] = sum;
+        }
+      }
+    }
+  }
 }
 
 int main(int argc, char ** argv)
@@ -240,12 +264,14 @@ int main(int argc, char ** argv)
   //float kernels[M][C][K][K];
   //float output[M][W][H];
   
-float *** image, **** kernels, *** output;
+  float *** image, **** kernels, *** output;
   float *** control_output;
-  long long mul_time;
+  long long mul_time1, mul_time2;
   int width, height, kernel_order, nchannels, nkernels;
-  struct timeval start_time;
-  struct timeval stop_time;
+  struct timeval start_time1;
+  struct timeval stop_time1;
+  struct timeval start_time2;
+  struct timeval stop_time2;
 
   if ( argc != 6 ) {
     fprintf(stderr, "Usage: conv-harness <image_width> <image_height> <kernel_order> <number of channels> <number of kernels>\n");
@@ -278,22 +304,26 @@ float *** image, **** kernels, *** output;
 
   //DEBUGGING(write_out(A, a_dim1, a_dim2));
 
+  gettimeofday(&start_time1, NULL);
   /* use a simple multichannel convolution routine to produce control result */
   multichannel_conv(image, kernels, control_output, width,
                     height, nchannels, nkernels, kernel_order);
+  gettimeofday(&stop_time1, NULL);
 
   /* record starting time of team's code*/
-  gettimeofday(&start_time, NULL);
+  gettimeofday(&start_time2, NULL);
 
   /* perform student team's multichannel convolution */
   team_conv(image, kernels, output, width,
                     height, nchannels, nkernels, kernel_order);
 
   /* record finishing time */
-  gettimeofday(&stop_time, NULL);
-  mul_time = (stop_time.tv_sec - start_time.tv_sec) * 1000000L +
-    (stop_time.tv_usec - start_time.tv_usec);
-  printf("Team conv time: %lld microseconds\n", mul_time);
+  gettimeofday(&stop_time2, NULL);
+  mul_time1 = (stop_time1.tv_sec - start_time1.tv_sec) * 1000000L +
+    (stop_time1.tv_usec - start_time1.tv_usec);
+  mul_time2 = (stop_time2.tv_sec - start_time2.tv_sec) * 1000000L +
+    (stop_time2.tv_usec - start_time2.tv_usec);
+  printf("     Dave time: %lld microseconds  Team conv time: %lld microseconds\n", mul_time1, mul_time2);
 
   DEBUGGING(write_out(output, nkernels, width, height));
 
