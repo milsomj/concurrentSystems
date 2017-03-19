20aa9160b63937c74169273d75ea5e4ff743b320
milsomj <milsomj@tcd.ie>
2017-03-19 17:21:34 +0000

    Vectorised and parallelised where kernel_order ==1

--- conv-harness.c
+++ conv-harness.c
@@ -230,32 +230,30 @@ void team_conv(float *** image, float **** kernels, float *** output,
 {
   int h, w, x, y, c, m;
 	if(kernel_order == 1){
-    printf("%s\n", "OKAY");
+    __m128 a4;
+    __m128 b4;
+    __m128 c4;
+    __m128 sum4;
+    float * temp = malloc(4*sizeof(float));
+    #pragma omp parallel for private(m,w,h,c,sum4,a4,b4,c4,sum,temp,output) collapse(4) //Need to revise the private variables
 		for ( m = 0; m < nkernels; m++ ) {
 		  for ( w = 0; w < width; w++ ) {
 		    for ( h = 0; h < height; h++ ) {
-		      float sum;
-          float temp[] = {0.0,0.0,0.0,0.0};
-          __m128 sum4 = _mm_loadu_ps(&temp[0]);
-          __m128 a4;
-          __m128 b4;
-          __m128 c4;
+          sum4 = _mm_setzero_ps();
 		      for ( c = 0; c < nchannels-3; c+=4 ) {
-		          a4 = _mm_loadu_ps(&image[w][h][c]);
-              b4 = _mm_loadu_ps(&kernels[m][c][0][0]);
+		          a4 = _mm_load_ps(&image[w][h][c]);
+              b4 = _mm_load_ps(&kernels[m][c][0][0]);
               c4 = _mm_mul_ps(a4,b4);
               sum4 = _mm_add_ps(sum4,c4);
-              //_mm_storeu_ps(&temp[c],c4);
 		      }
-          _mm_storeu_ps(&temp[0],sum4);
-          sum = temp[0] + temp[1] + temp[2] + temp[3];
+          _mm_store_ps(&temp[0],sum4);
+          float sum = temp[0] + temp[1] + temp[2] + temp[3];
 					output[m][w][h] = sum;
 		    }
 		  }
 		}
 	}
 	else{
-    printf("%s\n", "NOT OKAY");
 		for ( m = 0; m < nkernels; m++ ) {
 		  for ( w = 0; w < width; w++ ) {
 		    for ( h = 0; h < height; h++ ) {
